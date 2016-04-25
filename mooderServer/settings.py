bd68d5e334e122751a95c49bed1912684f5d1d09
Bartosz Cwynar <cwynar.bartosz@gmail.com>
2016-04-25 23:11:12 +0200

    first reset

--- mooderServer/settings.py
+++ mooderServer/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -51,6 +53,11 @@ MIDDLEWARE_CLASSES = [
 ]
 
 ROOT_URLCONF = 'mooderServer.urls'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'PAGE_SIZE': 10
+}
 
 TEMPLATES = [
     {
